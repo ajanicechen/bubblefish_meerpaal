738d2e0e76d67c180dfa522391203a6330aa8c7e
Annie Chen <a.janice.chen@gmail.com>
2022-08-29 12:36:00 +0200

    initial

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>meerpaal</title>
+        <link rel="stylesheet" href="css/style.css">
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"> -->
+    </head>
+
+    <body>
+            <div class="container">
+                <div class="item1">
+                    <h1>Welkom!</h1>
+                    <h2>Leg aan in onze haven</h2>
+                    <p class="intro">Op VMBO De Meerpaal valt niemand buiten de boot, wat je verhaal ook is. 
+                        Of je nou moeite hebt met stilzitten of je last hebt van dyslexie, 
+                        bij ons is het normaal dat iedereen bijzonder is. Door de vertrouwensband 
+                        van leerling tot leerkracht en van conciërge tot ouder creëren wij een 
+                        veilige omgeving waarin iedereen kan opbloeien. Leg aan in onze haven!</p>
+                </div>
+                <div class="item2">
+                    <h3>Actueel</h3>
+                    <h4>Om te noteren</h4>
+                    <h4>Nieuws</h4>
+                </div>
+                <div class="item3">
+                    <h5>Onderwijsvisie</h5>
+                    <h6>Veilige leeromgeving</h6>
+                    <p>De Meerpaal biedt kinderen die in cognitief of sociaal-emotioneel opzicht 
+                        ‘anders’ zijn, een veilige en uitdagende leeromgeving. Daarom gaat ons 
+                        onderwijs net een stapje verder dan op andere scholen. Zo hechten wij aan 
+                        overzichtelijkheid en aandacht voor persoonlijke ontwikkeling. Bovendien heeft 
+                        iedere leerling eigen leerdoelen, om onderwijs op maat te creëren.</p>
+                </div>
+
+
+                <img class="right-img" src="resources/Rectangle_177.png">
+            </div>
+    </body>
+   
+
+</html>
