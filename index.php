364cb9a1cb36a8036ee58f18b6334cd8c399dac4
Annie Chen <a.janice.chen@gmail.com>
2022-08-29 15:48:42 +0200

    header and footer

--- index.php
+++ index.php
@@ -8,39 +8,102 @@
     <head>
         <meta charset="UTF-8">
         <title>meerpaal</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
-        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"> -->
     </head>
 
     <body>
-            <div class="container">
-                <div class="item1">
-                    <h1>Welkom!</h1>
-                    <h2>Leg aan in onze haven</h2>
-                    <p class="intro">Op VMBO De Meerpaal valt niemand buiten de boot, wat je verhaal ook is. 
-                        Of je nou moeite hebt met stilzitten of je last hebt van dyslexie, 
-                        bij ons is het normaal dat iedereen bijzonder is. Door de vertrouwensband 
-                        van leerling tot leerkracht en van conciërge tot ouder creëren wij een 
-                        veilige omgeving waarin iedereen kan opbloeien. Leg aan in onze haven!</p>
-                </div>
-                <div class="item2">
-                    <h3>Actueel</h3>
-                    <h4>Om te noteren</h4>
-                    <h4>Nieuws</h4>
-                </div>
-                <div class="item3">
-                    <h5>Onderwijsvisie</h5>
-                    <h6>Veilige leeromgeving</h6>
-                    <p>De Meerpaal biedt kinderen die in cognitief of sociaal-emotioneel opzicht 
-                        ‘anders’ zijn, een veilige en uitdagende leeromgeving. Daarom gaat ons 
-                        onderwijs net een stapje verder dan op andere scholen. Zo hechten wij aan 
-                        overzichtelijkheid en aandacht voor persoonlijke ontwikkeling. Bovendien heeft 
-                        iedere leerling eigen leerdoelen, om onderwijs op maat te creëren.</p>
-                </div>
-
-
-                <img class="right-img" src="resources/Rectangle_177.png">
+        <div class="content-container">
+            <div class="header">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Waar ben je naar opzoek?" aria-label="Search">
+                    <button class="btn" type="submit">Zoeken</button>
+                </form>
             </div>
+            <div class="item1">
+                <h1>Welkom!</h1>
+                <h2>Leg aan in onze haven</h2>
+                <p class="intro">
+                    Op VMBO De Meerpaal valt niemand buiten de boot, wat je verhaal ook is. 
+                    Of je nou moeite hebt met stilzitten of je last hebt van dyslexie, 
+                    bij ons is het normaal dat iedereen bijzonder is. Door de vertrouwensband 
+                    van leerling tot leerkracht en van conciërge tot ouder creëren wij een 
+                    veilige omgeving waarin iedereen kan opbloeien. Leg aan in onze haven!
+                </p>
+            </div>
+            <div class="item2">
+                <h3>Actueel</h3>
+                <h4>Om te noteren</h4>
+                <h4>Nieuws</h4>
+            </div>
+            <div class="item3">
+                <h5>Onderwijsvisie</h5>
+                <h6>Veilige leeromgeving</h6>
+                <p>
+                    De Meerpaal biedt kinderen die in cognitief of sociaal-emotioneel opzicht 
+                    ‘anders’ zijn, een veilige en uitdagende leeromgeving. Daarom gaat ons 
+                    onderwijs net een stapje verder dan op andere scholen. Zo hechten wij aan 
+                    overzichtelijkheid en aandacht voor persoonlijke ontwikkeling. Bovendien heeft 
+                    iedere leerling eigen leerdoelen, om onderwijs op maat te creëren.
+                </p>
+
+                <h6>Onderdeel van Comenius</h6>
+                <p>
+                    De Meerpaal is onderdeel van scholengroep Comenius College. Onder Comenius College 
+                    vallen een aantal middelbare scholen in Rotterdam-Oost. Op haar beurt is Comenius 
+                    weer onderdeel van CVO Rotterdam, de vereniging voor Christelijk Voortgezet Onderwijs. 
+                    Onderdeel van het CVO is ook een shared-service-organisatie die veel van de 
+                    administratieve taken van De Meerpaal op zich neemt.
+                </p>
+            </div>
+            <div class="item4">
+                <h1>Ook het anker uitgooien?</h1>
+                <p>
+                    Heb je het advies vmbo-tl, -kb of -bb gekregen? En zoek je een school met extra begeleiding? 
+                    De Meerpaal is dan misschien een goede school voor jou. Check de pagina Ik ben nieuw om 
+                    te zien wat je van ons kan verwachten.
+                </p>
+            </div>
+            <div class="footer">
+                <div class="row align-items-start">
+                    <div class="col-4">
+                        <ul>
+                            <li>©2022 Calvijn</li>
+                            <li><a href="">Privacyverklaring</a></li>                                
+                            <li>Ontwerp Doordacht</li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul>
+                            <li>Contact Kreeftstraat 44</li>
+                            <li>3067 JV Rotterdam </li>
+                            <li>Op schooldagen open </li>
+                            <li>van 08.00 tot 16.30 uur </li>
+                            <br>
+                            <li>010 286 1313 </li>
+                            <li>info@vmbo-demeerpaal.nl</li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul>
+                            <li>Sitemap </li>
+                            <li>Welkom </li>
+                            <li>Ik ben nieuw</li> 
+                            <li>Mijn school </li>
+                            <li>Voor ouders </li>
+                            <li>Organisatie </li>
+                            <li>Nieuws </li>
+                            <li>Agenda </li>
+                            <li>Vacatures </li>
+                            <li>Contact </li>
+                            <li>Portaal↗ </li>
+                            <li>Comenius↗</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <img class="right-img" src="resources/Rectangle_177.png">
+        </div>
     </body>
    
 
